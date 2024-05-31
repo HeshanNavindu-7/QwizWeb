f0b62010dcc697900ad72a26c89cff156309a63d
Heshan Navindu <121144605+HeshanNavindu-7@users.noreply.github.com>
2024-05-31 21:09:24 +0530

    Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Quiz from './pages/Quiz/Quiz'
+
+
+
+import Home from './pages/Home/Home'
+
 
 function App() {
  
 
   return (
     <>
-<Quiz></Quiz>
+<Home></Home>
     </>
   )
 }
